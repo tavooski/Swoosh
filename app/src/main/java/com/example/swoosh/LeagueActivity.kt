cabba29a4a210319f06b32eac912b1a156df5f48
Gustavo <ruiz.gustavo97@gmail.com>
2022-12-14 16:17:06 -0800

    added data binding to activites

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,18 +1,66 @@
 package com.example.swoosh
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.example.swoosh.databinding.ActivityLeagueBinding
 
 class LeagueActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityLeagueBinding
+
+    private var selectedLeague = ""
+    //private val mensLeagueBttn: Button = findViewById(R.id.mensLeagueBttn)
+    //private val womensLeagueBttn: Button = findViewById(R.id.womensLeagueBttn)
+    //private val coedLeagueBttn: Button = findViewById(R.id.coedLeagueBttn)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_league)
+        binding = ActivityLeagueBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.mensLeagueBttn.setOnClickListener {
+            onMensClicked()
+        }
+        binding.womensLeagueBttn.setOnClickListener {
+            onWomensClicked()
+        }
+        binding.coedLeagueBttn.setOnClickListener {
+            onCoedClicked()
+        }
     }
 
     fun leagueNextClicked(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+        if (selectedLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun onMensClicked() {
+        binding.womensLeagueBttn.isChecked = false
+        binding.coedLeagueBttn.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClicked() {
+        binding.mensLeagueBttn.isChecked = false
+        binding.coedLeagueBttn.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClicked() {
+        binding.mensLeagueBttn.isChecked = false
+        binding.womensLeagueBttn.isChecked = false
+
+        selectedLeague = "co-ed"
     }
 }
--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -4,14 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.swoosh.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val getStartedBtn: Button = findViewById(R.id.getStartedBtn)
-        getStartedBtn.setOnClickListener {
+        // val getStartedBtn: Button = findViewById(R.id.getStartedBtn)
+        binding.getStartedBtn.setOnClickListener {
             val leagueIntent = Intent(this, LeagueActivity::class.java)
             startActivity(leagueIntent)
         }
